93622a87dd10670b24059bd7339047c0cc4711cd
Yomar Miranda <mirandayomar@gmail.com>
2020-06-30 14:41:36 -0500

    feat: v1.0.0

---
+++ docs/precache-manifest.99e2c6532fc3c38ad31573ba6bb750c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f3756d05add84afa741973684d3109",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a3004103b5be6423921",
+    "url": "/static/css/main.a48070df.chunk.css"
+  },
+  {
+    "revision": "3de35a795cee06d455f9",
+    "url": "/static/js/2.fb33ce99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb33ce99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3004103b5be6423921",
+    "url": "/static/js/main.900c0095.chunk.js"
+  },
+  {
+    "revision": "86ec7bb7ac2324fce23a",
+    "url": "/static/js/runtime-main.326c7b88.js"
+  },
+  {
+    "revision": "dfa6fe26556b45ce941bf6c72cbc8fd7",
+    "url": "/static/media/bg.dfa6fe26.jpg"
+  },
+  {
+    "revision": "1aaddf520ddd94f240b7f125d65a49a8",
+    "url": "/static/media/profile.1aaddf52.jpeg"
+  }
+]);
