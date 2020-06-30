fd572c4388513160822eadab22da1771c452fcda
Yomar Miranda <mirandayomar@gmail.com>
2020-06-29 19:58:37 -0500

    feat: create navigation header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Navigation></Navigation>
+    </>
   );
 }
 
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <div className="navigation-header">
+        <a href="#" className="navigation-header__link">Yomar Miranda</a> 
+      </div>
+    </nav>
+  )
+}
+
+export default Navigation;
